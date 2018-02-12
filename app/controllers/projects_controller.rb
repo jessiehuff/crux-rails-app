344b82a0c94e24c5c6c7523984e69102406b78fe
Jessie Huff <huffjd1@gmail.com>
2018-02-12 17:47:42 +0000

    Restful routes in Project Controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,46 @@
 class ProjectsController < ApplicationController
+
+  def index
+    @projects = Projects.all
+  end
+
+  def show
+    @project = Project.find_by(id: params[:id])
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      @project
+      render :new
+    end
+  end
+
+  def edit
+    @project = Project.find_by(id: params[:id])
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to project_path(@project)
+    else
+      render :show
+    end
+  end
+
+  def destroy
+    @project.destroy
+    reidrect_to projects_path
+  end
+
+private
+  def project_params
+    params.require(:project).permit(:title, :description)
+  end
 end
