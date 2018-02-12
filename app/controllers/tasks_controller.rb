2b250c064ea45d352a5196eba3db67d5c4b627a2
Jessie Huff <huffjd1@gmail.com>
2018-02-12 18:14:01 +0000

    Updating Tasks Controller again

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task
+  before_action :set_project
 
   def index
     @tasks = @project.tasks
@@ -19,20 +20,30 @@ class TasksController < ApplicationController
   end
 
   def show
+    @project = @task.project
   end
 
   def edit
+
   end
 
   def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
   end
 
   def destroy
+    @task.destroy
+    redirect_to project_tasks_path(@project)
   end
 
 private
   def set_tasks
     @task = Task.find_by(id: params[:id])
+  end
+
+  def set_project
+    @project = Project.find_by(id: params[:project_id])
   end
 
   def tasks_params(:task).permit(:title, :description, :assigned_to, :created_at, :completed_at, :status, :project_id)
