dfe362c496929ca28f65482b6a6a85e6c11014d4
Jessie Huff <huffjd1@gmail.com>
2018-02-12 19:02:13 +0000

    Users controller new and create methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,25 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
   before_action :set_user, only: [:show, :edit, :update]
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    if auth
+      @user = User.find_or_create_with_oauth(auth)
+      session[:user_id] = @user.id
+      redirect_to home_url
+    else
+      @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to home_path
+      else
+        render :new
+      end
+    end
+  end
 
 private
   def user_params
