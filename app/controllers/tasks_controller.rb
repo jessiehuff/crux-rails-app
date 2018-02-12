238d809af776dbb5e6d1b9488cc7e7f13daa2539
Jessie Huff <huffjd1@gmail.com>
2018-02-12 18:02:20 +0000

    Adding tasks params to TasksController

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,6 @@
 class TasksController < ApplicationController
+
+private
+  def tasks_params(:task).permit(:title, :description, :assigned_to, :created_at, :completed_at, :status, :project_id)
+  end
 end
