d0f167a139872d3383ffa220c72d516ef3276843
Jessie Huff <huffjd1@gmail.com>
2018-02-12 17:14:30 +0000

    Adding status to Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,9 @@ class Task < ActiveRecord::Base
   belongs_to :project
 
   validates :title, :description, :created_at, :completed_at, :status, :project_id, presence: true
+
+  enum status: [:active, :complete]
+  scope :complete, -> {where(status: 1)}
+  scope :active, -> {where(status: 0)}
+
 end
