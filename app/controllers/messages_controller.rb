2a12a913c7b71f1dfd836967bb1480da432de25a
Jessie Huff <huffjd1@gmail.com>
2018-02-12 18:36:54 +0000

    Adding JSON

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,11 @@ class MessagesController < ApplicationController
   before_action :set_project
 
   def index
-
+    @messages = @project.messages.reverse
+    respond_to do |format|
+      format.hmtl {render :index}
+      format.json {render: @messages}
+    end
   end
 
   def new
@@ -14,22 +18,32 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     if @message.save
       @project = @message.project
-      reidrect_to project_messages_path(@project)
+      redirect_to project_messages_path(@project)
     else
       render :new
     end
   end
 
   def show
+    @project = @message.project
+    respond_to do |format|
+      format.html {render :show}
+      format.json {render json: @message}
+    end
   end
 
   def edit
+    @project = @message.project
   end
 
   def update
+    @message.update(message_params)
   end
 
   def destroy
+    @project = @message.project
+    @message.destroy
+    redirect_to project_messages_path(@project)
   end
 
 private
