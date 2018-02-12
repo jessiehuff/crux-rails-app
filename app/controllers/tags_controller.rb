91eda095cd86a368e3651f1a6e8e69367731526c
Jessie Huff <huffjd1@gmail.com>
2018-02-12 17:53:55 +0000

    Tags Controller updated

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,23 @@
 class TagsController < ApplicationController
+
+  def index
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      redirect_to tags_path
+    else
+      render :index
+    end
+  end
+
+  def destroy
+
+  end
+
+private
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
 end
