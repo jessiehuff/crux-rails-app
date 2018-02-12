637fcb6dc92a0f59f663c4d86731664e8dd98f0e
Jessie Huff <huffjd1@gmail.com>
2018-02-12 16:58:38 +0000

    Adding Project Associations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,7 @@
 class Project < ActiveRecord::Base
+  has_and_belongs_to_many :users
+  has_many :messages
+  has_many :tasks
+  has_many :project_tags
+  has_many :tags, through: :project_tags
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_and_belongs_to_many :projects
+  has_many :messages
+
+  def all_projects
+    self.projects
+  end
+
 end
