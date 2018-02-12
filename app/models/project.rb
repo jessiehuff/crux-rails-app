10cb87cb0e62d50eabd9f1243b368ffef8e6a133
Jessie Huff <huffjd1@gmail.com>
2018-02-12 17:09:03 +0000

    Added a few validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,6 @@ class Project < ActiveRecord::Base
   has_many :tasks
   has_many :project_tags
   has_many :tags, through: :project_tags
+
+  validates :title, :content, presence: true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ActiveRecord::Base
   belongs_to :project
+
+  validates :title, :description, :created_at, :completed_at, :status, :project_id, presence: true
 end
