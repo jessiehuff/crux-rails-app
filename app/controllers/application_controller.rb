e21490aadac992436b71ddb8160224d4f7b34c6f
Jessie Huff <huffjd1@gmail.com>
2018-02-13 16:47:10 +0000

    Trying to get Devise to work with Sign Ups

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,10 @@ class ApplicationController < ActionController::Base
   def set user
     @user = current_user
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:username, :first_name, :last_name, :email, :password, :password_confirmation)
+    end
+  end
 end
