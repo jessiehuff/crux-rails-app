de010f5d13edabced5eded2304c1b5e05bc0fc79
Jessie Huff <huffjd1@gmail.com>
2018-02-20 15:59:07 +0000

    Adding user profile but need to work on routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
+  resources :users, :only => [:show]
 
   resources :projects do
     resources :messages
