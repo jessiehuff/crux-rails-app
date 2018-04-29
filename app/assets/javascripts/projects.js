b8579e9176f0888d580ec966edd8b2484396b9e1
jessiehuff <huffjd1@gmail.com>
2018-04-28 21:14:47 -0400

    Display Project Messages Javascript

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -15,3 +15,39 @@ function attachProjectListeners() {
   });
 }
 
+function loadMessages(element){
+  $.ajax({
+    method: "GET", 
+    dataType: "json", 
+    url: element.href
+  })
+  .success(function(data){
+    let messages = data.messages 
+    if (messages.length === 0){
+      let $header = $("div.project-messages strong"); 
+      $header.html("No Message(s)")
+    } else {
+      let $header = $("div.project-messages strong"); 
+      $header.html("Message(s):")
+      let $ul = $("div.project-messages ul")
+      messages.forEach(message => {
+        let newMessage = new Message(message)
+        let messageHtml = newMessage.formatMessage() 
+
+        $ul.append(messageHtml)
+      });
+    }
+  });
+}
+
+function Message(message) {
+  this.title = message.title 
+  this.content = message.content 
+  this.id = message.id
+}
+
+Message.prototype.formatMessage = function() {
+  let messageHtml = `
+  <li><a href='/messages/${this.id}'>${this.name}</a></li>`
+  return messageHtml; 
+}
